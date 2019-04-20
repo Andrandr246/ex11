1ac4712166a0818506020de52231fce06c1dc3bf
Andrandr246 <andrandr246@gmail.com>
2019-04-20 21:12:03 +0300

    Initial commit

---
+++ src/Ex11.java
@@ -0,0 +1,9 @@
+public class Ex11 {
+    public static void main(String args[]){
+        int arr[] = {2, 34, 88, 19, 64, 42, 90, 12, 89, 14, 17};
+        int i = 18, j;
+        for(j=0; j<arr.length; j++){
+            if (arr[j]<=i) System.out.println(arr[j]);
+        }
+    }
+}
